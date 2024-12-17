9807fe9aba4e9ba1db30a7c7d74c8e079c8486ab
Morten Jagd Christensen <mortenjc@jcaps.com>
2024-12-17 23:14:05 +0100

    refactor to combine both solutions

--- 2024/day17/solution.py
+++ 2024/day17/solution.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import sys
 import re
 import math
+import random
 
 
 
@@ -26,7 +27,6 @@ regs = {}
 
 for reg in init.split('\n'):
     reg = reg.split(':')
-    print(reg)
     var = reg[0][-1]
     val = int(reg[1])
     regs[var] = val
@@ -34,24 +34,20 @@ for reg in init.split('\n'):
 pg = prg.split(':')[1].split(',')
 pg = list(map(int, pg))
 
-assert len(pg) == 14
-
 OP = {0:'adv', 1:'bxl', 2:'bst', 3:'jnz', 4:'bxc', 5:'out', 6:'bdv', 7:'cdv'}
 
 
-SEEN = set()
-def run(pg, regs):
+
+matches = 0
+def run(pg, regs, part2):
+    n = regs['A']
+    global matches
     pc = 0
     output = []
     while True:
-        if pc >= len(pg):
-            print('normal exit')
-            break
-
         opc = pg[pc]
         opr = pg[pc + 1]
 
-        lval = opr
         if opr <= 3:
             cval = opr
             reg = 'I'
@@ -60,65 +56,69 @@ def run(pg, regs):
             cval = regs[reg]
         else:
             cval = -1
-            pass
 
-        print(f'pc {pc}: opcode {opc} ({OP[opc]}), lval {lval}, reg {reg}, cval {cval}')
-
-        if (pc, regs['A'], regs['B'], regs['C']) in SEEN:
-            print('loop')
-            break
-        SEEN.add((pc, regs['A'], regs['B'], regs['C']))
-        
-        if opc == 0: # ADV
-            denum = 2**cval
-            num = regs['A']
-            regs['A'] = num//denum
-            #print(f'adv: A = {num}/{denum}')
-
-        elif opc == 1: # BXL
-            regs['B'] = regs['B'] ^ lval
-            #print(f'bxl: B = {regs['B']} xor {lval}')
-
-        elif opc == 2: #BST
+        if opc == 0: # adv
+            regs['A'] = regs['A']//2**cval
+        elif opc == 1: # bxl
+            regs['B'] = regs['B'] ^ opr
+        elif opc == 2: # bst
             regs['B'] = cval % 8
-            #print(f'bst: B = {cval} % 8')
-
-        elif opc == 3: # JNZ
+        elif opc == 3: # jnz
             if regs['A'] != 0:
-                #print(f'jnz: pc = pc + {lval}')
-                pc = lval - 2
-
-        elif opc == 4: # BXC
-            #print(f'bxc: {regs['B']} ^ {regs['C']}')
+                pc = opr - 2
+        elif opc == 4: # bxc
             regs['B'] = regs['B'] ^ regs['C']
-
         elif opc == 5: # out
-            #print(f'out: {cval%8}')
+            if part2:
+                if cval%8 != pg[len(output)]: # mismatch
+                    newmatch = False
+                    if len(output) > matches:
+                        matches = len(output)
+                        newmatch = True
+                    return output, newmatch
             output.append(cval%8)
 
         elif opc == 6: # bdv
-            #print(f'bdv: B = {regs['A']}//{(2**cval)}')
             regs['B'] = regs['A']//(2**cval)
-
         elif opc == 7: # cdv
-            #print(f'cdv: C = {regs['A']}//{(2**cval)}')
             regs['C'] = regs['A']//(2**cval)
-
         else:
-            #print(opc)
             assert False
         pc += 2
-        #print(regs)
+        if pc >= len(pg):
+            break
 
-    return output
+    return output, False
+
+
 
 
 tregs = regs.copy()
-#tregs['A'] = 117440
-res = run(pg, tregs)
-print(res)
-S1 = ','.join(list(map(str,res)))
+res, newbest = run(pg, tregs, part2=False)
 
+S1 = ','.join([str(x) for x in res])
+
+
+i = 0
+cst = 0
+pow = 0
+while True:
+    tregs = regs.copy()
+    ast = i * 8**pow + cst
+    tregs['A'] = ast
+
+    res, newbest = run(pg, tregs, part2=True)
+    if newbest and matches > 3:
+        pow = matches
+        ostr = str(oct(ast))
+        cst = int('0o' + ostr[-matches:], 8)
+        print(f'new: i * 8**{pow} + {oct(cst)}')
+        i = 0
+
+    if res == pg:
+        S2 = ast
+        break
+    i += 1
 
 print("------------- A -------------")
 print('S1 ', S1)
