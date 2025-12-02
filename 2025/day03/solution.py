975a1c8dd3410b2f132c9d3b81b4c7a43718b79e
Morten Jagd Christensen <mortenjc@jcaps.com>
2025-12-02 19:54:07 +0100

    create day03 folder

---
+++ 2025/day03/solution.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+
+import numpy as np
+import sys
+
+
+infile = sys.argv[1] if len(sys.argv) > 1 else 'test.txt'
+print("<<{}>>".format(infile))
+
+S1 = 0
+S2 = 0
+
+with open(infile) as fin:
+    lines = ((fin.read().strip()).split('\n'))
+
+for line in lines:
+    print(line)
+
+
+print("------------- A -------------")
+print('S1 ', S1)
+print("------------- B -------------")
+print('S2 ', S2)
+print("-----------------------------")
