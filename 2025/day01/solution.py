ba5106b87a46dc91ee4f655354196d82280a900b
Morten Jagd Christensen <mortenjc@jcaps.com>
2025-12-02 19:52:42 +0100

    day02

--- 2025/day01/solution.py
+++ 2025/day01/solution.py
@@ -15,37 +15,8 @@ with open(infile) as fin:
     lines = ((fin.read().strip()).split('\n'))
 
 
-angle = 50
-
 for line in lines:
-    dir = line[0]
-    ang = int(line[1:])
-
-    assert dir in ['L', 'R']
-    assert 0 < ang
-
-    if dir == 'L':
-        step = -1
-    else:
-        step = 1
-
-    for i in range(abs(ang)):
-        angle += step
-        angle %= 100
-        if angle == 0:
-            S2 += 1
-
-    if angle == 0:
-        S2 -= 1
-        S1 += 1
-
-
-
-
-S2 += S1
-
-
-
+    print(line)
 
 
 print("------------- A -------------")
